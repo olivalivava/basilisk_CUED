d23693a543ce91ad2d274a8fe1a9ccc1129670fd
olivalivava <oliva.huang@outlook.com>
2024-11-21 14:21:17 +0000

    simulate without gravity

---
+++ src/coalescence_ND.c
@@ -0,0 +1,92 @@
+#include "grid/multigrid.h"
+#define dimension 3
+
+#include "navier-stokes/centered.h"
+#define mu(f)  (1./(clamp(f,0,1)*(1./mu1 - 1./mu2) + 1./mu2))
+#include "two-phase.h"
+#include "vof.h"
+#include "tension.h"
+#include "view.h"
+//#include "axi.h"
+
+#define LEVEL 8
+
+double We = 61.4;
+double Re = 296.5;
+double B = 0.0;
+double R = 0.0168; //does not run for 0.000168
+
+
+#define rho1c 762.0 //tetradecane
+#define rho2c 1.251 //nitrogen
+#define sigmac 0.0276 //tetradecane
+#define MUR 97.7
+double runtime = 0.5; //set runtime length
+
+//double R1 = R; //set the radius for the left droplet (R1<1.)
+//double R2 = R; //set the radius for the right (R2<1.)
+//double uvel = 0.5; //set colliding speed (uniform velocity ??)
+//#include "two-phase-generic.h"
+
+// Open boundaries at the left and right sides
+u.n[left] = neumann(0.);    // Free flow: no gradient in normal velocity
+u.t[left] = neumann(0.);    // Free slip: no gradient in tangential velocity
+
+u.n[right] = neumann(0.);
+u.t[right] = neumann(0.);
+
+
+int main()
+{
+  size (15.*R);
+  init_grid(64); // Base resolution
+  origin (-L0/2., -L0/2. , -L0/2.);     //changed the origin
+  double uvelc = sqrt((We*sigmac)/(rho1c*2*R));
+  rho1 = rho1c;               //kg/m^3
+  rho2 = rho2c;               //kg/m^3
+  mu1 = (rho1*uvelc*2*R)/Re;  // (Pa * s)
+  mu2 = mu1/MUR;              //Pa s
+  f.sigma = sigmac;           //N/m
+  TOLERANCE = 1e-4 [*];       //defult 1e04
+  run();
+}
+
+event init (t = 0)
+{
+  fraction (f, max (- (sq(x + 2*R) + sq(y) + sq(z)- sq(R)),
+		                - (sq(x - 2*R) + sq(y) + sq(z) - sq(R))));
+  foreach() {
+      double uvelc = sqrt((We*sigmac)/(rho1c*2*R));
+      u.x[] = - sign(x)*f[] * uvelc; //how to assign velocity to each droplet?
+  }
+}
+
+// event adapt(i++) {
+//   foreach() {
+//     refine(f[] > 0.01 && f[] < 0.99 && level < LEVEL);
+//   }
+// }
+
+// event acceleration (i++) {
+//   face vector av = a;
+//   foreach_face(y)
+//     av.y[] -= 0.98;   //gravity = 9.81
+// }
+
+
+// event adapt (i++) {
+//   double uemax = 1e-2;
+//   adapt_wavelet ({f,u}, (double[]){0.001,uemax,uemax,uemax}, LEVEL, 5);
+// }
+
+
+event movie (t += 0.004; t <= runtime)
+{
+  clear();
+  //view (width = 20*R, height = 10*R);
+  //squares ("u.x", spread = -1, linear = true); //removed this
+  draw_vof ("f");
+
+  box();
+  save ("movie_ND.mp4");
+}
