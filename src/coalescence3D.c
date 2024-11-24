c79a118880278595a6651625abb0fb499a39216a
olivalivava <oliva.huang@outlook.com>
2024-11-24 21:40:08 +0000

    case h: rhor=0.7, mur=0.82, uvel=50

--- src/coalescence3D.c
+++ src/coalescence3D.c
@@ -22,13 +22,13 @@ double R = 0.000168; // R1 = R/Rr, 0.000168
 #define mu2c 2.128e-3 //tetradecane
 #define sigmac 0.0276 //tetradecane
 #define RHOR 0.7 // 1.6417e-3, 0.06
-#define MUR 2.0 //8.186e-3, 1.25
+#define MUR 0.82 //8.186e-3, 1.25
 
 //#define RHOR 0.06
 //#define MUR 1.25
 //#define Rr 1000
-double runtime = 0.0002; //set runtime length
-double uvel = 100.;
+double runtime = 0.0003; //set runtime length
+double uvel = 50.;
 
 //double R1 = R; //set the radius for the left droplet (R1<1.)
 //double R2 = R; //set the radius for the right (R2<1.)
