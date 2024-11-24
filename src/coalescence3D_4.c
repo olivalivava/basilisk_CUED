43e584ad2310c917efb925d70f09b1c663b484dc
olivalivava <oliva.huang@outlook.com>
2024-11-24 21:06:00 +0000

    case k: rhor=0.7, mur=5.0, uvel=20, removed z-axis

--- src/coalescence3D_4.c
+++ src/coalescence3D_4.c
@@ -21,12 +21,12 @@ double R = 0.000178; // R1 = R/Rr
 #define rho2c 758.0 //tetradecane
 #define mu1c 1.787e-5 //nitrogen
 #define mu2c 2.128e-3 //tetradecane
-#define RHOR 0.75 //rho1/rho2, 0.25
-#define MUR 1.05//mu1/mu2, 1.05
-#define Rr 1000
+#define RHOR 0.70 //rho1/rho2, 0.25
+#define MUR 5.0//mu1/mu2, 1.05
+//#define Rr 1000
 
 double runtime = 0.0002; //set runtime length 0.003 good for whole period
-double uvel = 35.;
+double uvel = 20.;
 //double R1 = R; //set the radius for the left droplet (R1<1.)
 //double R2 = R; //set the radius for the right (R2<1.)
 //double uvel = 0.5; //set colliding speed (uniform velocity ??)
@@ -44,7 +44,7 @@ int main()
 {
   size (10.*R);
   init_grid(64); // Base resolution
-  origin (-L0/2., -L0/2. , -L0/2.);     //changed the origin
+  origin (-L0/2., -L0/2. , 0.);     //changed the origin
   rho2 = (We * sigmac)/(2*R*uvel*uvel);          //kg/m^3
   rho1 = rho2*RHOR; 
   mu2 = rho2*uvel*(2*R)/Re;     
