1ab5eddbda4ddd0c1ce3ffba1d162d77cfda8d8d
olivalivava <oliva.huang@outlook.com>
2024-11-24 19:38:12 +0000

    uvel = 17.5, increased velocity

--- src/coalescence3D.c
+++ src/coalescence3D.c
@@ -18,15 +18,17 @@ double R = 0.000168; // R1 = R/Rr, 0.000168
 
 #define rho1c 1.251 //nitrogen (outside)
 #define rho2c 762.0 //tetradecane (inside)
-#define mu1c 1.787e-5 //nitrogen
+#define mu1c 1.787e-5//nitrogen 1.787e-5
 #define mu2c 2.128e-3 //tetradecane
 #define sigmac 0.0276 //tetradecane
 #define RHOR 1.251/762.0
 #define MUR 1.787e-5/2.128e-3
+
 //#define RHOR 0.06
 //#define MUR 1.25
 //#define Rr 1000
-double runtime = 0.001; //set runtime length
+double runtime = 0.003; //set runtime length
+double uvel = 17.5;
 
 //double R1 = R; //set the radius for the left droplet (R1<1.)
 //double R2 = R; //set the radius for the right (R2<1.)
@@ -43,13 +45,17 @@ double runtime = 0.001; //set runtime length
 
 int main()
 {
-  size (15.*R);
-  init_grid(128); // Base resolution
+  size (10.*R);
+  init_grid(64); // Base resolution
   origin (-L0/2., -L0/2. , -L0/2.);     //changed the origin
-  rho1 = rho1c;
-  rho2 = rho2c;               //kg/m^3
-  mu1 = mu1c;
-  mu2 = mu2c;              //Pa s
+  rho2 = (We * sigmac)/(2*R*uvel*uvel);               //kg/m^3
+//  rho1 = rho1c;
+  // mu2 = mu2c;
+  // mu1 = mu1c;
+  rho1 = rho2*RHOR;      //used the parameters given on the website
+//  double uvel = sqrt((We*sigmac)/(rho2*2*(R)));
+  mu2 = rho2*uvel*(2*R)/Re;                      //Pa s
+  mu1 = mu2*MUR;
   f.sigma = sigmac;           //N/m
   TOLERANCE = 1e-4 [*];       //defult 1e04
   run();
@@ -58,10 +64,10 @@ int main()
 event init (t = 0)
 {
   double X = B*(2*(R));
-  fraction (f, max (- (sq(x + 2*R) + sq(y) + sq(z)- sq(R)),
-		                - (sq(x - 2*R) + sq(y) + sq(z-X) - sq(R))));
+  fraction (f, max (- (sq(x + 1.3*R) + sq(y) + sq(z)- sq(R)),
+		                - (sq(x - 1.3*R) + sq(y) + sq(z-X) - sq(R))));
   foreach() {
-      double uvel = sqrt((We*sigmac)/(rho2*2*(R)));
+//      double uvel = sqrt((We*sigmac)/(rho2*2*(R)));
       u.x[] = - sign(x)*f[] * uvel; //how to assign velocity to each droplet?
   }
 }
@@ -86,14 +92,14 @@ event init (t = 0)
 // }
 
 event logfile (i++) {
-  double uvel = sqrt((We*sigmac)/(rho2*2*(R)));
+//  double uvel = sqrt((We*sigmac)/(rho2*2*(R)));
   fprintf(stderr, "%g, %g, %g, %g, %g, %g, %g, %g, %g, %g", 
                     t, dt,rho1, rho2, mu1, mu2, f.sigma, uvel, perf.t, perf.speed);
   putchar ('\n');
   fflush (stdout);
 }
 
-event movie (t += 1.0e-6; t <= runtime)
+event movie (t += 1.0e-5; t <= runtime)
 {
   clear();
   //view (width = 20*R, height = 10*R);
